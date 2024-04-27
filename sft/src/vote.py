cbfe1282e84af856f31fbb211fe458e947983187
DandinPower <tomhot246@gmail.com>
2024-04-27 09:56:24 +0800

    Add weight vote

--- sft/src/vote.py
+++ sft/src/vote.py
@@ -1,32 +1,81 @@
 import pandas as pd
-from collections import Counter
 from argparse import ArgumentParser, Namespace
+from dataclasses import dataclass
+import os
 
 
-def vote(files: list[str]):
-    print(files)
-    # Read all files into dataframes
+class ScoreRanker:
+    def __init__(self):
+        self.scores: dict[str, float] = {}
+
+    def add_score(self, answer: str, weight: float):
+        if answer in self.scores:
+            self.scores[answer] += weight
+        else:
+            self.scores[answer] = weight
+
+    def get_highest_score_answer(self):
+        return max(self.scores, key=self.scores.get)
+
+    def __repr__(self) -> str:
+        return str(self.scores)
+
+
+@dataclass
+class VoteData:
+    id: str
+    score_ranker: ScoreRanker
+
+    # def vote(files: list[str]):
+    #     print(files)
+    #     # Read all files into dataframes
+    #     dfs = [pd.read_csv(file) for file in files]
+    #     # Concatenate all dataframes
+    #     df = pd.concat(dfs)
+    #     # Group by index and apply voting function to rating
+    #     result = df.groupby('id')['answer'].apply(
+    #         lambda x: Counter(x).most_common(1)[0][0])
+    #     return result
+
+
+def create_vote_data(files: list[str], weights: list[float]) -> list[VoteData]:
     dfs = [pd.read_csv(file) for file in files]
-    # Concatenate all dataframes
-    df = pd.concat(dfs)
-    # Group by index and apply voting function to rating
-    result = df.groupby('id')['answer'].apply(
-        lambda x: Counter(x).most_common(1)[0][0])
-    return result
+    vote_data = []
+    for df_index, df in enumerate(dfs):
+        for row_index, row in enumerate(df.iterrows()):
+            _, row = row
+            index = str(row['id'])
+            if df_index == 0:
+                vote_data.append(
+                    VoteData(id=index, score_ranker=ScoreRanker()))
+            vote_data[row_index].score_ranker.add_score(
+                str(row['answer']), weights[df_index])
+    return vote_data
+
+
+def write_csv_file(vote_data: list[VoteData], output_file: str):
+    dirname = os.path.dirname(output_file)
+    os.makedirs(dirname, exist_ok=True)
+
+    with open(output_file, 'w') as f:
+        f.write('id,answer\n')
+        for vote in vote_data:
+            f.write(f'{vote.id},{vote.score_ranker.get_highest_score_answer()}\n')
 
 
 def main(args: Namespace):
-    vote_csv = vote(args.vote_files)
-    with open(args.output_file, 'w') as f:
-        f.write('id,answer\n')
-        for index, rating in vote_csv.items():
-            f.write(f'{index},{rating}\n')
+    print(args.weights)
+    vote_datas = create_vote_data(args.vote_files, args.weights)
+    print(vote_datas[200])
+    write_csv_file(vote_datas, args.output_file)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--vote_files", nargs="+",
                         help="List of files to vote")
+    parser.add_argument("--weights", nargs="+", type=float,
+                        help="List of weights for each file")
     parser.add_argument("--output_file", type=str, required=True)
     args = parser.parse_args()
     main(args)
--- sft/vote.sh
+++ sft/vote.sh
@@ -1,6 +1,8 @@
-VOTE_FILE="predict/llama_3_8b_lora_completion_only.csv predict/breeze_7b_lora_completion_only.csv predict/breeze_7b_lora_completion_only_5_epochs.csv"
-OUTPUT_FILE="predict/llama_breeze_breeze5.csv"
+VOTE_FILE="predict/llama_breeze_breeze5.csv predict/llama_breeze_breeze5_gemma_weighted.csv predict/llama_breeze_breeze5_gemma_taide_mistral_weighted.csv predict/llama_breeze_gemma_taide.csv predict/llama_breeze_gemma_taide_mistral.csv predict/gemma_7b_lora_completion_only.csv predict/taide_7b_lora_completion_only.csv predict/mistral_7b_lora_completion_only.csv"
+WEIGHTS="0.918996 0.916992 0.912999 0.910996 0.91 0.899996 0.846999 0.794998 0.750998"
+OUTPUT_FILE="predict/ultimate_vote_v3.csv"
 
 python src/vote.py \
     --vote_files $VOTE_FILE \
+    --weights $WEIGHTS \
     --output_file $OUTPUT_FILE
